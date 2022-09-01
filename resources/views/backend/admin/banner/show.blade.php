3bee712c75c1cc9a5e3c83123af26c82b3c31098
Rajib-hossain-cse-rmstu <rajibraju771@gmail.com>
2022-09-01 11:42:55 +0600

    admin banner and customer breadcrumb add

--- resources/views/backend/admin/banner/show.blade.php
+++ resources/views/backend/admin/banner/show.blade.php
@@ -1,5 +1,28 @@
 @extends('backend.master')
 
+@section('admin_breadcrumb')
+    <!-- [ breadcrumb ] start -->
+    <div class="page-header">
+        <div class="page-block">
+            <div class="row align-items-center">
+                <div class="col-md-12">
+                    <div class="page-header-title">
+                        <h5 class="m-b-10">Admin Dashboard</h5>
+                    </div>
+                    <ul class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="{{route('admin.dashboard')}}"><i class="feather icon-home"></i></a>
+                        </li>
+                        <li class="breadcrumb-item"><a href="{{route('admin.banner.list')}}">Banner</a></li>
+                        <li class="breadcrumb-item"><a href="#">View Banner</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- [ breadcrumb ] end -->
+@endsection
+
+
 @section('admin_dashboard_content')
     <div class="row">
             <!-- [ sample-page ] start -->
--- resources/views/backend/admin/customer/edit.blade.php
+++ resources/views/backend/admin/customer/edit.blade.php
@@ -4,6 +4,30 @@
 <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
 @endsection
+
+@section('admin_breadcrumb')
+    <!-- [ breadcrumb ] start -->
+    <div class="page-header">
+        <div class="page-block">
+            <div class="row align-items-center">
+                <div class="col-md-12">
+                    <div class="page-header-title">
+                        <h5 class="m-b-10">Admin Dashboard</h5>
+                    </div>
+                    <ul class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="{{route('admin.dashboard')}}"><i class="feather icon-home"></i></a>
+                        </li>
+                        <li class="breadcrumb-item"><a href="{{route('admin.customer.list')}}">Customer</a></li>
+                        <li class="breadcrumb-item"><a href="#">Edit Customer</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- [ breadcrumb ] end -->
+@endsection
+
+
 @section('admin_dashboard_content')
 
 <div class="row">
