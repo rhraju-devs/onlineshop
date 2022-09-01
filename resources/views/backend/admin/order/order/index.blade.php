3cbd0ccaf8c4c8c34e2e6d5c42a7f3d801930da7
Rajib-hossain-cse-rmstu <rajibraju771@gmail.com>
2022-09-01 12:13:21 +0600

    Order details list breadcrumb add

--- resources/views/backend/admin/order/order/index.blade.php
+++ resources/views/backend/admin/order/order/index.blade.php
@@ -5,6 +5,28 @@
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css">
 @endsection
 
+@section('admin_breadcrumb')
+    <!-- [ breadcrumb ] start -->
+    <div class="page-header">
+        <div class="page-block">
+            <div class="row align-items-center">
+                <div class="col-md-12">
+                    <div class="page-header-title">
+                        <h5 class="m-b-10">Admin Dashboard</h5>
+                    </div>
+                    <ul class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="{{route('admin.dashboard')}}"><i class="feather icon-home"></i></a>
+                        </li>
+                        <li class="breadcrumb-item"><a href="{{route('admin.orderdetails.list')}}">Order Details</a></li>
+                        <li class="breadcrumb-item"><a href="#">Odred Details List</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- [ breadcrumb ] end -->
+@endsection
+
 @section('admin_dashboard_content')
 
 
